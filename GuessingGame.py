77be15e2ca12dc27a8c4e5d0ee0e76b1ba5ffe16
Samantha M <samdm@posteo.net>
2017-12-08 12:05:06 -0800

    Almost done

--- GuessingGame.py
+++ GuessingGame.py
@@ -15,12 +15,13 @@ computers_num = random.randint(start,end)
 tries = 5
 for i in range(0,tries):
                 guess = int(input("Guess a number between %s and %s!" %(start,end)))
-#Recall our if statements from last class!
-if guess == computers_num: #The question marks won't work!
-                print("The computer chose %s, you chose %s" %(computers_num, guess))
-elif guess < computers_num:
-                print("Your number is too low!")
-elif guess > computers_num:
-                print("Your number is too high!")
-#TO run multiple times we will use a for loop!               
+                if guess == computers_num: #win
+                                print("The computer chose %s, you chose %s" %(computers_num, guess))
+                                #our game is almost done!
+                                break#Stops when we leave the game!
+                elif guess < computers_num:
+                                print("Your number is too low!")
+                elif guess > computers_num:
+                                print("Your number is too high!")
+              
                 
